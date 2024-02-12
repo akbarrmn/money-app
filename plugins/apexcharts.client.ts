b2bdb7072e94b0b8576bd23f03e1ddf74487c969
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-12 22:00:07 +0700

    feat: adding apexchart library and setup plugins

---
+++ plugins/apexcharts.client.ts
@@ -0,0 +1,7 @@
+// @ts-ignore
+import VueApexCharts from "vue3-apexcharts"
+
+export default defineNuxtPlugin(nuxtApp => {
+const apexchart = VueApexCharts;
+nuxtApp.vueApp.component("apexchart", apexchart);
+})
