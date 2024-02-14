def423d0f1ff879019a407b97ad2ac5b26c9844b
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-14 10:53:46 +0700

    refactor: remove and move into diff dirs

--- utils/setColorTheme.ts
+++
@@ -1,6 +0,0 @@
-type Theme = 'light' | 'dark'
-export default function setColorTheme (theme: string) {
-  window.localStorage.setItem('theme',theme)
-  const getTheme:string | null = window.localStorage.getItem('theme')
-  useColorMode().preference = getTheme ?? 'dark'
-}
