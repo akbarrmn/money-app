ed11e848ff2d77a1cacc2f4a4551f544bab92778
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-14 10:48:55 +0700

    feat: initialize shadcn usable components

--- components/navbar.vue
+++ components/navbar.vue
@@ -6,14 +6,18 @@
         <h1 class="text-black dark:text-white">Money App</h1>
       </div>
       <div class="hidden lg:flex gap-5">
-        <NuxtLink to="#" class="text-[#4D72F6] dark:text-white">Overview</NuxtLink>
+        <NuxtLink to="#" class="text-[#4D72F6] dark:text-white"
+          >Overview</NuxtLink
+        >
         <NuxtLink to="#" class="text-black dark:text-white">Reports</NuxtLink>
         <NuxtLink to="#" class="text-black dark:text-white">Planning </NuxtLink>
       </div>
     </div>
     <div class="flex gap-4 items-center">
       <button
-        @click="setColorTheme($colorMode.preference == 'dark' ? 'light': 'dark')"
+        @click="
+          useColorTheme($colorMode.preference == 'dark' ? 'light' : 'dark')
+        "
         class="rounded-full hidden md:block outline outline-[#B4B8B9]/30 hover:bg-gray-400/50 p-2"
       >
         <svg
@@ -48,44 +52,113 @@
         </svg>
       </button>
 
-      <button
-        class="rounded-full hidden md:block outline outline-[#B4B8B9]/30 hover:bg-gray-400/50 p-2"
-      >
-        <svg
-          class="w-6 h-6 text-gray-600 dark:text-white "
-          aria-hidden="true"
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
+      <Popover>
+        <PopoverTrigger as-child>
+          <button
+            @click="setOpenNotification()"
+            class="relative rounded-full hidden md:block outline outline-[#B4B8B9]/30 hover:bg-gray-400/50 p-2"
+          >
+            <svg
+              class="w-6 h-6 text-gray-600 dark:text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M12 5.4V3m0 2.4a5.3 5.3 0 0 1 5.1 5.3v1.8c0 2.4 1.9 3 1.9 4.2 0 .6 0 1.3-.5 1.3h-13c-.5 0-.5-.7-.5-1.3 0-1.2 1.9-1.8 1.9-4.2v-1.8A5.3 5.3 0 0 1 12 5.4ZM8.7 18c.1.9.3 1.5 1 2.1a3.5 3.5 0 0 0 4.6 0c.7-.6 1.3-1.2 1.4-2.1h-7Z"
+              />
+            </svg>
+            <div
+              v-bind:class="{
+                'absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold text-white border-2 border-white rounded-full -top-2 -end-2 dark:border-gray-900 bg-red-500':
+                  !isOpen,
+                'absolute inline-flex items-center justify-center w-6 h-6 text-xs font-bold bg-gray-400/50 text-white border-2 border-[#B4B8B9]/30 rounded-full -top-2 -end-2 dark:border-gray-900':
+                  isOpen,
+              }"
+            >
+              {{ getReports.length }}
+            </div>
+          </button>
+        </PopoverTrigger>
+        <PopoverContent
+          class="w-72 dark:bg-[#313234] border-2 border-[#B4B8B9]/30 rounded-2xl p-6"
         >
-          <path
-            stroke="currentColor"
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            stroke-width="2"
-            d="M12 5.4V3m0 2.4a5.3 5.3 0 0 1 5.1 5.3v1.8c0 2.4 1.9 3 1.9 4.2 0 .6 0 1.3-.5 1.3h-13c-.5 0-.5-.7-.5-1.3 0-1.2 1.9-1.8 1.9-4.2v-1.8A5.3 5.3 0 0 1 12 5.4ZM8.7 18c.1.9.3 1.5 1 2.1a3.5 3.5 0 0 0 4.6 0c.7-.6 1.3-1.2 1.4-2.1h-7Z"
-          />
-        </svg>
-      </button>
-      <button
-        class="flex gap-3 items-center hover:bg-gray-300/50 p-2 rounded-lg"
-      >
-        <img
-          src="~/assets/images/user.png"
-          alt="logo"
-          width="40"
-          class="filter drop-shadow-md"
-        />
-        <div class="flex flex-col items-start text-xs md:text-base dark:text-white">
-          <p>Akbar Rahmana</p>
-          <p>akbarrahmana123</p>
-        </div>
-      </button>
+          <div
+            v-for="i in getReports"
+            :key="i.id"
+            class="grid grid-cols-4 gap-2 text-sm lg:text-md"
+          >
+            <p class="dark:text-white truncate">{{ i.transaction_name }}</p>
+            <p class="col-span-2 text-gray-400">
+              {{ useDateParse(JSON.parse(JSON.stringify(i.createdAt))) }}
+            </p>
+            <p
+              v-bind:class="{
+                'text-green-500': i.transaction_type,
+                'text-red-500': !i.transaction_type,
+              }"
+            >
+              {{ useFormatCurrency(i.transaction_amount) }}
+            </p>
+          </div>
+        </PopoverContent>
+      </Popover>
+
+      <DropdownMenu>
+        <DropdownMenuTrigger >
+          <button
+          class="flex gap-3 items-center hover:bg-gray-300/50 p-2 rounded-lg"
+          >
+            <img
+              v-bind:src="getSingleUser.image"
+              alt="logo"
+              width="40"
+              class="filter drop-shadow-md rounded-full"
+            />
+            <div
+              class="flex flex-col items-start text-xs md:text-base dark:text-white"
+            >
+              <p>{{ getSingleUser.name }}</p>
+              <p>{{ getSingleUser.username }}</p>
+            </div>
+          </button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent class="w-36 dark:bg-[#313234] border-2 border-[#B4B8B9]/30 rounded-xl p-2">
+          <DropdownMenuItem class="cursor-pointer">
+            <button @click="logout" class="flex items-center w-full h-full">
+              <LogOut class="ml-4 h-4 w-4" />
+              <span class="ml-2">Log out</span>
+            </button>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   </nav>
 </template>
 
 <script lang="ts" setup>
-const route = useRoute()
-console.log(route)
+import { LogOut } from "lucide-vue-next";
+import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem } from "./ui/dropdown-menu";
+import { Popover, PopoverContent, PopoverTrigger } from "./ui/popover";
+
+const route = useRoute();
+const userStore = useUserStore();
+const reportStore = useMyReportsStore();
+const getSingleUser = userStore.filteredSingleUser;
+const getReports = reportStore.reports;
+const isOpen = computed(() => reportStore.isOpen);
+
+const setOpenNotification = () => {
+  reportStore.setOpen(true);
+};
+
+const logout = async () => {
+  await userStore.logoutUser();
+  navigateTo("/auth");
+};
 </script>
