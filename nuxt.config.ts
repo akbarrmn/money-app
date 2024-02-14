4b75f3cba31979a65793d64df11ed024c400e384
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-14 10:42:09 +0700

    feat: adding shadcn config into nuxt

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,14 +7,34 @@ export default defineNuxtConfig({
     "@pinia/nuxt",
     "@pinia-plugin-persistedstate/nuxt",
     "@nuxtjs/color-mode",
+    "shadcn-nuxt",
+    "@vee-validate/nuxt"
   ],
   pinia:{
     storesDirs: [
       './stores/**'
     ]
+  }, 
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  },
+  runtimeConfig:{
+    public:{
+      baseUrl:process.env.API_BASE_URI
+    }
   },
   colorMode:{
-    classSuffix:''
+    classSuffix:'',
+    preference:'light'
+
   },
   sourcemap:false
 })
