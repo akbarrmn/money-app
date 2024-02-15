d3e456e1726d8801c437b920e9fd86f383b545d5
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-15 08:29:48 +0700

    feat: adding shadcn components

--- plugins/apexcharts.client.ts
+++ plugins/apexcharts.client.ts
@@ -1,6 +1,7 @@
 // @ts-ignore
 import VueApexCharts from "vue3-apexcharts"
 
+//create apexchart plugin
 export default defineNuxtPlugin(nuxtApp => {
 const apexchart = VueApexCharts;
 nuxtApp.vueApp.component("apexchart", apexchart);
