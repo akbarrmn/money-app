82a494128396938fdafa1471dff2c484c720b8c2
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-15 08:32:25 +0700

    feat: update home page split component

--- pages/index.vue
+++ pages/index.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="container mx-auto pb-10">
+  <div class="container mx-auto pb-5">
     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 auto-rows-[190px] md:auto-rows-[200px] gap-4">
-      <LazyBalance :balance="useFormatCurrency(getUserData.balance)" />
+      <LazyBalance />
 
-      <LazyBudget :balance="useFormatCurrency(getUserData.balance)"/>
+      <LazyTransfer/>
 
       <LazyReportsMedium />
 
-      <LazyIncome :income="useFormatCurrency(getUserData.income)" />
+      <LazyIncome />
 
-      <LazyExpends :expends="useFormatCurrency(getUserData.expends)" />
+      <LazyExpends />
 
       <LazyReportsSmall />
 
@@ -19,15 +19,10 @@
 </template>
 
 <script lang="ts" setup>
+//defining page meta and set middlewares
 definePageMeta({
   middleware: ['not-authenticate']
 })
-
-const userStore = useUserStore()
-const getUserData = userStore.filteredSingleUser
-
-
-
 </script>
 
 <style></style>
