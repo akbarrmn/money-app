2ce41aaf81d4e786d806a6d13bfca3108425a21f
Akbar Rahmana <akbar.rahmana@gmail.com>
2024-02-15 08:33:21 +0700

    feat: adding report pages

---
+++ pages/report/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="container mx-auto pb-10">
+    <div class="auto-rows-[190px] md:auto-rows-[200px] gap-4">
+      <ReportsMedium/>
+      <ReportsSmall/>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  definePageMeta({
+    middleware: ['not-authenticate'],
+  })
+</script>
+
+<style>
+
+</style>
